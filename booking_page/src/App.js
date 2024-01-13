b3bb81c5208ba11fdd8e83afab0900349275273a
BeckyLashua <beckylashua@gmail.com>
2024-01-13 09:48:47 -0500

    Commiting entire basic app

---
+++ booking_page/src/App.js
@@ -0,0 +1,16 @@
+//import logo from './logo.svg';
+import Form from './Form';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Booking Form</h1>
+      </header>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
