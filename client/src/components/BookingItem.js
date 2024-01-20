a795184369e9ac7c7bad40a73f48b6a58740c7e9
BeckyLashua <beckylashua@gmail.com>
2024-01-20 12:20:44 -0600

    Fixed translation bug in BookingItem.js

--- client/src/components/BookingItem.js
+++ client/src/components/BookingItem.js
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
-
 import '../styles/BookingItem.css';
 
 function BookingItem( {appt} ) {
+  const { t } = useTranslation();
   return (
     <li data-testid='booking-item-1' key={appt.id}>
       <h4>{t('apptCardTitle')} {appt.id}</h4>
