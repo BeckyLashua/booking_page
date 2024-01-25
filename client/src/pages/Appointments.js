fb19243ea173651f0581b8e8cea0f6f478e2cc00
BeckyLashua <beckylashua@gmail.com>
2024-01-25 10:14:10 -0600

    Updated Apointments page to test connection with Django

--- client/src/pages/Appointments.js
+++ client/src/pages/Appointments.js
@@ -1,4 +1,6 @@
 import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import  axios  from 'axios';
 //import { useTranslation } from 'react-i18next';
 import BookingList from '../components/BookingList';
 import t from '../translations/en.json';
@@ -7,8 +9,21 @@ import '../App.css';
 
 
 function Appointments() {
-  const location = useLocation();
-  const { appts } = location.state;
+  //const location = useLocation();
+  //const { appts } = location.state;
+  const [appointments, setAppointments] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:8000/api/appointments/')
+        .then(response => {
+            // Handle the data
+            setAppointments(response.data);
+        })
+        .catch(error => {
+            console.error('There was an error!', error);
+        });
+}, []);
+
   /*let appts = [
     {id: 1, date: '7/03/24', time: '10:30am'},
     {id: 2, date: '9/30/24', time: '4:00pm'},
@@ -32,7 +47,7 @@ function Appointments() {
         </Link><br />
       </div>
       <h2>{t.appointmentsHeader}</h2>
-      <BookingList appts = {appts}/>
+      <BookingList appts = { appointments }/>
     </div>
   );
 }
