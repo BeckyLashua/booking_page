ef79ca92a8371c16854b26dd66d676dfd12f3781
BeckyLashua <beckylashua@gmail.com>
2024-01-31 11:05:00 -0600

    Updated model to match PostgreSQL database table

--- booking_page/appointments/models.py
+++ booking_page/appointments/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 class Appointment(models.Model):
     appt_id = models.IntegerField(primary_key=True)
-    firstname = models.CharField(max_length=100)
-    lastname = models.CharField(max_length=100)
-    phone = models.CharField(max_length=15)
-    email = models.EmailField()
-    date = models.DateField()
-    time = models.TimeField()
+    client_first_name = models.CharField(max_length=100)
+    client_last_name = models.CharField(max_length=100)
+    client_phone = models.CharField(max_length=15)
+    client_email = models.EmailField()
+    appt_date = models.DateField()
+    appt_time = models.TimeField()
 
     def __str__(self):
         return f"{self.firstname} {self.lastname} with email {self.email} on {self.date} at {self.time}"
